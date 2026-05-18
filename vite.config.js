90d2b1b30c0d53b7562dcb8fc81c58c39f3f9681
Ishita-2611 <23bec045@nith.ac.in>
2026-05-18 13:11:36 +0530

    configured deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio_final",
-  build: {
-    chunkSizeWarningLimit: 1600,
-  },
+  base: '/portfolio_final',
 })
